5cefa254aeec32fd2182bfd016a2919d8b995bd5
xDaniil <dan.mednikov@icloud.com>
2019-09-09 19:59:16 +0300

    1.1/done

---
+++ homework1.1.js
@@ -0,0 +1,8 @@
+let name = "Dan";
+const birthDate = 2001;
+
+let greetings = (name) => `Welcome ${name}`;
+
+console.log(greetings('Daniil'));
+console.log(greetings(name));
+console.log(greetings(birthDate));
